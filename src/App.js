d6f1f6d95afb02dc30d4b66506371886bbd35dce
Narendra Rajpoot <nrajpoot1146@gmail.com>
2022-03-20 22:31:19 +0530

    Add Work experience

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import { Component } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import './App.css';
 import Me from './Me';
 import Projects from './projects/Projects';
 import WorkExperiences from './WorkExperiences';
 
-function hello() {
-  console.log('hello');
-}
-
-class App extends Component {
+class AppInner extends Component{
   constructor(props) {
     super(props);
     this.state = {
       x: 10,
-      section: <Me/>
+      section: <Me deviceInfo={this.props.deviceInfo}/>
     };
-
     this.onClicksMe = this.onClicksMe.bind(this);
     this.onClickProjects = this.onClickProjects.bind(this);
     this.onClickWorkExperience = this.onClickWorkExperience.bind(this);
@@ -23,7 +19,7 @@ class App extends Component {
 
   onClicksMe(e) {
     // console.log(this.state.x); 
-    this.setState((state, props) => {return{x : state.x+1, section: <Me/>}});
+    this.setState((state, props) => {return{ section: <Me deviceInfo={this.props.deviceInfo}/>}});
   }
 
   onClickProjects(e) {
@@ -52,11 +48,12 @@ class App extends Component {
       <div className='AppBar'>
         <span className='Title'>
           {/* Narendra Rajpoot */}
+          {this.props.deviceInfo.isDesktopOrLaptop}
         </span>
         <div className='MenuItems'>
           <span onClick={this.onClicksMe} className='MenuItem'>Me!</span>
           <span onClick={this.onClickProjects} className='MenuItem'>Projects</span>
-          <span onClick={hello} className='MenuItem'>Skills</span>
+          <span onClick={()=>{}} className='MenuItem'>Skills</span>
           <span onClick={this.onClickWorkExperience} className='MenuItem'>Work Experience</span>
         </div>
       </div>
@@ -66,6 +63,18 @@ class App extends Component {
     </div>
     );
   };
+}
+
+function App() {
+  var deviceInfo = {
+    isDesktopOrLaptop: useMediaQuery({ query: '(min-width: 1224px)'}),
+    isBigScreen: useMediaQuery({ query: '(min-width: 1824px)' }),
+    isTabletOrMobile: useMediaQuery({ query: '(max-width: 1224px)' }),
+    isPortraiT: useMediaQuery({ query: '(orientation: portrait)' }),
+    isRetina: useMediaQuery({ query: '(min-resolution: 2dppx)' })
+  }
+  
+  return <AppInner deviceInfo = {deviceInfo}/>
 }
 
 export default App;
--- src/Me.js
+++ src/Me.js
@@ -1,19 +1,46 @@
 import './Me.css'
 
-function Me() {
-    return (
+import { Component } from 'react';
 
-        <div className="Summary">
-            <div style={{display: 'inline-block', position: 'relative', left: 100}}>
-                <span className='Hi'>Hi, I'm</span>
-                <span>Narendra Rajpoot</span>
-                <div className='' style={{ 'fontSize': '20px' }}>Assistant System Engineer</div>
-                <div className='' style={{ 'fontSize': '20px' }}>@ Tata Consultancy Services</div>
+
+
+class Me extends Component {
+    constructor(props) {
+        super(props);
+
+        this.style = { display: "inline-block", position: 'relative', left: 100 }
+
+        if (this.props.deviceInfo.isTabletOrMobile == true) {
+            this.style.display = 'block';
+            // this.style.left = '0';
+            // this.style.top = '30px';
+            this.style.position = 'static';
+            this.style.textAlign = 'center';
+            this.style.verticalAlign = 'middle'
+        }
+    }
+
+    render() {
+        return (
+            <div className="Summary">
+                {/* {this.props.deviceInfo.isDesktopOrLaptop ? "fdfd" : "123"} */}
+                <div style={this.style}>
+                    <span className='Hi'>Hi, I'm</span>
+                    <span>Narendra Rajpoot</span>
+                    <div className='' style={{ 'fontSize': '20px' }}>Assistant System Engineer</div>
+                    <div className='' style={{ 'fontSize': '20px' }}>@ Tata Consultancy Services</div>
+                </div>
+
+                <div style={{
+                    position: this.props.deviceInfo.isTabletOrMobile ? 'block' : 'relative',
+                    fontSize: this.props.deviceInfo.isTabletOrMobile ? '30px' : '180px',
+                    display: this.props.deviceInfo.isTabletOrMobile ? 'block' : 'inline-block',
+                    textAlign: 'center',
+                    left: this.props.deviceInfo.isTabletOrMobile ? '0' : '30%'
+                }}>&lt;/&gt;</div>
             </div>
 
-            <div style={{position: 'relative', fontSize: 180, display: 'inline-block', textAlign: 'center', left: '30%'}}>&lt;/&gt;</div>
-        </div>
-
-    );
+        );
+    }
 }
 export default Me;
